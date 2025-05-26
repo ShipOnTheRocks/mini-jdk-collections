01005cb18f9c231d6550adbefb94310036d874c2
chenzhou09 <chenzhou09@meituan.com>
2025-05-26 19:51:56 +0800

    代码初始化

---
+++ mini-jdk-collections-test/src/main/java/com/ship/collection/list/MyArrayList.java
@@ -0,0 +1,16 @@
+package com.ship.collection.list;
+
+import java.util.ArrayList;
+
+/**
+ * MyArrayList
+ *
+ * @author mac
+ * @version 1.0 2025/5/26 19:48
+ */
+public class MyArrayList {
+
+    public static void main(String[] args) {
+        new ArrayList<>();
+    }
+}
---
+++ mini-jdk-collections-test/src/main/java/com/ship/collection/map/MyHashMap.java
@@ -0,0 +1,17 @@
+package com.ship.collection.map;
+
+import java.util.HashMap;
+
+/**
+ * MyHashMap
+ *
+ * @author mac
+ * @version 1.0 2025/5/26 19:42
+ */
+public class MyHashMap {
+
+    public static void main(String[] args) {
+        new HashMap<>();
+    }
+
+}
